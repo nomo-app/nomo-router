7cc2120de08e134ed6b3cd05a6966f0ce27c0b02
dev2 <dev2@nomo.app>
2024-04-15 12:50:09 +0200

    Prepare for pub.dev

--- route_gen/lib/anotations.dart
+++
@@ -1,3 +0,0 @@
-class AppRoutes {
-  const AppRoutes();
-}
--- route_gen/lib/builder.dart
+++
@@ -1,8 +0,0 @@
-library route_gen.builder;
-
-import 'package:build/build.dart';
-import 'package:route_gen/src/route_generator.dart';
-import 'package:source_gen/source_gen.dart';
-
-Builder routeBuilder(BuilderOptions options) =>
-    SharedPartBuilder([RouteGenerator()], 'route_builder');
